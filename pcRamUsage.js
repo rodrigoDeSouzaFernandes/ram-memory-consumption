e7299da7dfcfadb27e7f6d4cd7222077692d22ae
rodrigo <rdesouza2008@gmail.com>
2022-07-04 15:02:38 -0300

    cria arquivo que consulta informações do pc do usuario e retorna no console

---
+++ pcRamUsage.js
@@ -0,0 +1,24 @@
+const os = require("os");
+
+setInterval(() => {
+  const { arch, platform, totalmem, freemem } = os;
+
+  const convertBytesToMegaBytes = (bytes) => parseInt(bytes / 1048576);
+
+  const tram = convertBytesToMegaBytes(totalmem()),
+    fram = convertBytesToMegaBytes(freemem()),
+    usage = (fram / tram) * 100;
+
+  const stats = {
+    OS: platform(),
+    Arch: arch(),
+    TotalRAM: `${tram} MB`,
+    FreeRAM: `${fram} MB`,
+    Usage: `${usage.toFixed(2)}%`
+  };
+
+  console.clear()
+  console.table(stats);
+
+  exports.stats = stats;
+}, 1000);
